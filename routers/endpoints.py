6202d132c1ba7685ed74c95cd0339296b7c54313
Alok Gaur <alokgaur200@gmail.com>
2025-09-28 19:24:15 +0530

    organize file

--- routers/endpoints.py
+++ routers/endpoints.py
@@ -1,17 +1,23 @@
-from fastapi import FastAPI, status, Request
-
-app = FastAPI()
+from fastapi import APIRouter, status, Request
+import credentials
+import json
+router = APIRouter(tags=['Web Hooks'])
 
 # WebHook to get the latest mail id
 # https://mail-management.ngrok.io/mail-hook
 # ngrok http 8000 --domain=mail-management.ngrok.io
 # ngrok http --url=openly-fluent-dogfish.ngrok-free.app 8000
-@app.post("/mail-hook")
+@router.post("/mail-hook")
 async def get_mail(request:Request):
     body = await request.json()
     message_data = body.get("message", {}).get("data")
     if message_data:
         import base64
-        decoded = base64.b64decode(message_data).decode("utf-8")
+        
+        creds = credentials.get_google_credentials()
+        decoded = json.loads(base64.b64decode(message_data).decode("utf-8"))
         print("New mail notification:", decoded)
+        
+        print("\n\nFetching message ids....")
+        message_id = credentials.mail_by_id(creds, decoded.get("historyId"))
     return {"status": "received"}
